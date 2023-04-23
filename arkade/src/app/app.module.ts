963ff409cd26b8e545398f75017d413e020122a2
Kade H <kadeiszman@gmail.com>
2023-04-22 22:02:01 -0500

    Add initial navbar and introduction components

--- arkade/src/app/app.module.ts
+++ arkade/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule,
