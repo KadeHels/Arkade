477ec321529e5eddfbe00da5aa07dceeb92ba865
Kade H <kadeiszman@gmail.com>
2023-04-23 18:30:13 -0500

    Update logo color to transition smoothly

--- arkade/src/app/components/introduction/introduction.component.ts
+++ arkade/src/app/components/introduction/introduction.component.ts
@@ -9,5 +9,26 @@ export class IntroductionComponent {
 
   text = 'Kade Helseth - Full Stack Developer -';
 
+  index = 0;
+  logoColor: any = 'red';
+  logoColors = ['#537D8D', '#CE8D66', '#913212', '#1F2041', '#2D444D'];
+
+  constructor() {
+    this.logoColor = this.logoColors[0];
+
+    this.loopLogoColors();
+  }
+
+  loopLogoColors() {
+    setTimeout(() => {
+      this.changeLogoColor();
+      this.loopLogoColors();
+    }, 10000);
+  }
+
+  changeLogoColor() {
+    this.index++;
+    this.logoColor = this.logoColors[this.index % this.logoColors.length];
+  }
 
 }
